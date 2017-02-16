512b0ac050729a701ec4de599d03f8ba323a4269
Jeff Ober <jeffober@gmail.com>
2017-02-16 09:11:14 -0500

    Start on docs

--- lib/Proc/tored/Pool.pm
+++ lib/Proc/tored/Pool.pm
@@ -1,4 +1,15 @@
 package Proc::tored::Pool;
+# ABSTRACT: Managed worker pool with Proc::tored and Parallel::ForkManager
+
+=head1 NAME
+
+Proc::tored::Pool
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
 
 use strict;
 use warnings;
@@ -24,11 +35,27 @@ our @EXPORT = (
   )
 );
 
-sub pool    ($%)   { Proc::tored::Pool::Loop->new(name => shift, @_); }
-sub hire    ($@)   { workers => shift, @_ }
-sub on      ($@)   { 'on_' . shift, @_ }
-sub call    (&@)   { @_ }
-sub pending ($)    { $_[0]->pending }
-sub process (&$;$) { $_[1]->assign($_[0], $_[2]) };
+=head1 EXPORTED SUBROUTINES
+
+=head2 pool
+
+=head2 capacity
+
+=head2 on
+
+=head2 call
+
+=head2 pending
+
+=head2 process
+
+=cut
+
+sub pool     ($%)   { Proc::tored::Pool::Loop->new(name => shift, @_); }
+sub capacity ($@)   { workers => shift, @_ }
+sub on       ($@)   { 'on_' . shift, @_ }
+sub call     (&@)   { @_ }
+sub pending  ($)    { $_[0]->pending }
+sub process  (&$;$) { $_[1]->assign($_[0], $_[2]) };
 
 1;
